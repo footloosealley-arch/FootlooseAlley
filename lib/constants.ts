ef0a605ed33573db6866ff8c501f87b26b6c200e
Footloose Alley <footloosealley@gmail.com>
2026-08-03 19:27:42 +0530

    Sync app version to 4.2.0

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const APP = {
   NAME: "Footloose Alley Studio Manager",
   SHORT_NAME: "Footloose Alley",
-  VERSION: "3.18.0",
+  VERSION: "4.2.0",
 } as const;
 
 export const STUDENT_STATUS = [
