f794a4bb73d3175852f296bdb5aadc5e1360b1a3
Footloose Alley <footloosealley@gmail.com>
2026-08-04 17:13:58 +0530

    Add safe deletion for studio expenses v5.5.1

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const APP = {
   NAME: "Footloose Alley Studio Manager",
   SHORT_NAME: "Footloose Alley",
-  VERSION: "5.5.0",
+  VERSION: "5.5.1",
 } as const;
 
 export const STUDENT_STATUS = [
