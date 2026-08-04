0adc6f0daae174011f455d5bbe99aa6549b9be1d
Footloose Alley <footloosealley@gmail.com>
2026-08-04 09:24:34 +0530

    Add event discount coupon management

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const APP = {
   NAME: "Footloose Alley Studio Manager",
   SHORT_NAME: "Footloose Alley",
-  VERSION: "4.7.1",
+  VERSION: "4.7.2",
 } as const;
 
 export const STUDENT_STATUS = [
